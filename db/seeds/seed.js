563ee3ae2fc6fe8ea3112a64eaa25bc9e707ede2
Alice-Claire <aliceclaireh@hotmail.co.uk>
2019-06-25 15:31:12 +0100

    add migrate and returning to seeds

--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -8,18 +8,24 @@ const {
 const { formatDate, formatComments, makeRefObj } = require('../utils/utils');
 
 exports.seed = function(knex, Promise) {
-  const topicsInsertions = knex('topics').insert(topicData);
-  // .return('*') ????
-  const usersInsertions = knex('users').insert(userData);
-
-  return Promise.all([topicsInsertions, usersInsertions])
+  return knex.migrate
+    .rollback()
+    .then(() => knex.migrate.latest())
     .then(() => {
-      const formattedArticles = formatDate(articleData);
-      return knex('articles').insert(formattedArticles);
-    })
-    .then(articleRows => {
-      const articleRef = makeRefObj(articleRows);
-      const formattedComments = formatComments(commentData, articleRef);
-      return knex('comments').insert(formattedComments);
+      const topicsInsertions = knex('topics').insert(topicData);
+      const usersInsertions = knex('users').insert(userData);
+
+      return Promise.all([topicsInsertions, usersInsertions])
+        .then(() => {
+          const formattedArticles = formatDate(articleData);
+          return knex('articles')
+            .insert(formattedArticles)
+            .returning('*');
+        })
+        .then(articleRows => {
+          const articleRef = makeRefObj(articleRows);
+          const formattedComments = formatComments(commentData, articleRef);
+          return knex('comments').insert(formattedComments);
+        });
     });
 };
