fb9c61c8c8ab9c7c4ee2ba923016a5342c2c9aea
Alice-Claire <aliceclaireh@hotmail.co.uk>
2019-06-25 12:51:33 +0100

    add makeRefObj func and tests

--- db/utils/utils.js
+++ db/utils/utils.js
@@ -8,6 +8,12 @@ exports.formatDate = list => {
   return newList;
 };
 
-exports.makeRefObj = list => {};
+exports.makeRefObj = articles => {
+  const result = {};
+  for (let article of articles) {
+    result[article.title] = article.article_id;
+  }
+  return result;
+};
 
 exports.formatComments = (comments, articleRef) => {};
--- spec/utils.spec.js
+++ spec/utils.spec.js
@@ -45,6 +45,37 @@ describe('formatDate', () => {
   });
 });
 
-describe('makeRefObj', () => {});
+describe('makeRefObj', () => {
+  it('returns an object', () => {
+    const list = [];
+    expect(makeRefObj(list)).to.be.an('object');
+  });
+  it('does not mutate the original array', () => {
+    const list = [
+      {
+        article_id: 1,
+        title: 'Title 1'
+      }
+    ];
+    const listValues = [
+      {
+        article_id: 1,
+        title: 'Title 1'
+      }
+    ];
+    makeRefObj(list);
+    expect(list).to.eql(listValues);
+  });
+  it('takes and array of objects and returns a reference object with keys of each items title, and values being each items corresponding id', () => {
+    const list = [
+      { article_id: 1, title: 'Title 1' },
+      { article_id: 2, title: 'Title 2' },
+      { article_id: 3, title: 'Title 3' }
+    ];
+    const actual = makeRefObj(list);
+    const expected = { 'Title 1': 1, 'Title 2': 2, 'Title 3': 3 };
+    expect(actual).to.eql(expected);
+  });
+});
 
 describe('formatComments', () => {});
