515b8a6c13460740ee13ead4964d9fe746b322a3
Alice-Claire <aliceclaireh@hotmail.co.uk>
2019-06-26 16:38:37 +0100

    add routers index

---
+++ routers/index.js
@@ -0,0 +1,4 @@
+exports.apiRouter = require('./apiRouter');
+exports.topicsRouter = require('./topicsRouter');
+exports.usersRouter = require('./usersRouter');
+exports.articlesRouter = require('./articlesRouter');
