77914ef62ac8abf25855aaffbb7ba3844e1cf550
Alice-Claire <aliceclaireh@hotmail.co.uk>
2019-06-26 15:01:17 +0100

    add getTopics logic

--- controllers/topics.js
+++ controllers/topics.js
@@ -1,5 +1,9 @@
+const { fetchTopics } = require('../models/topics');
+
 const getTopics = (req, res, next) => {
-  console.log('getting topics');
+  fetchTopics(req.query)
+    .then(topics => res.status(200).send({ topics }))
+    .catch(next);
 };
 
 module.exports = getTopics;
--- spec/app.spec.js
+++ spec/app.spec.js
@@ -1,4 +1,4 @@
-const { app } = require('../app');
+const app = require('../app');
 const chai = require('chai');
 const expect = chai.expect;
 const request = require('supertest')(app);
@@ -6,16 +6,16 @@ const { connection } = require('../db/connection');
 
 describe('/api', () => {
   after(() => {
-    client.end();
+    connection.destroy();
   });
-  // describe('/topics', () => {
-  //   it('GET all topics', () => {
-  //     return request
-  //       .get('/api/topics')
-  //       .expect(200)
-  //       .then(({ body: { topics } }) => {
-  //         expect(1).to.equal(1);
-  //       });
-  //   });
-  // });
+  describe('/topics', () => {
+    it('GET all topics', () => {
+      return request
+        .get('/api/topics')
+        .expect(200)
+        .then(({ body: { topics } }) => {
+          expect(topics[0]).to.contain.keys('slug', 'description');
+        });
+    });
+  });
 });
