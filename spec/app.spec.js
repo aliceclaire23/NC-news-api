4383289a97cf63a7d30e0ee2151215d77fc36bc2
Alice-Claire <aliceclaireh@hotmail.co.uk>
2019-06-26 16:38:52 +0100

    add GET user test

--- spec/app.spec.js
+++ spec/app.spec.js
@@ -5,6 +5,7 @@ const request = require('supertest')(app);
 const { connection } = require('../db/connection');
 
 describe('/api', () => {
+  beforeEach(() => connection.seed.run());
   after(() => {
     connection.destroy();
   });
@@ -15,6 +16,28 @@ describe('/api', () => {
         .expect(200)
         .then(({ body: { topics } }) => {
           expect(topics[0]).to.contain.keys('slug', 'description');
+        });
+    });
+  });
+  describe('/users/:username', () => {
+    it('GET user by usename', () => {
+      return request
+        .get('/api/users/jessjelly')
+        .expect(200)
+        .then(({ body: { user } }) => {
+          expect(user[0]).to.contain.keys('username', 'avatar_url', 'name');
+          expect(user.length).to.equal(1);
+        });
+    });
+  });
+  describe('/articles/:article_id', () => {
+    it('GET article by article_id', () => {
+      return request
+        .get('/api/articles/jessjelly')
+        .expect(200)
+        .then(({ body: { user } }) => {
+          expect(user[0]).to.contain.keys('username', 'avatar_url', 'name');
+          expect(user.length).to.equal(1);
         });
     });
   });
