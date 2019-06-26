e5d9e408d37503fed354e5eaea574934b2ca6df2
Alice-Claire <aliceclaireh@hotmail.co.uk>
2019-06-26 16:36:54 +0100

    add 500 error handling

--- app.js
+++ app.js
@@ -5,4 +5,9 @@ const apiRouter = require('./routers/apiRouter');
 app.use(express.json());
 app.use('/api', apiRouter);
 
+// 500 errors
+app.use((err, req, res, next) => {
+  res.status(500).send({ msg: 'Internal Server Error' });
+});
+
 module.exports = app;
