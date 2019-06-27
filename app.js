6b729076c8e700421eba29ec927c7f7a376d28c5
Alice-Claire <aliceclaireh@hotmail.co.uk>
2019-06-27 13:02:28 +0100

    add 400 code and console log for 500 errors

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.use('/api', apiRouter);
 
 // handle psql 400 errors
 app.use((err, req, res, next) => {
-  const codes = ['22P02', '42703', '23502'];
+  const codes = ['22P02', '23502', '23503', '42703'];
   if (codes.includes(err.code)) {
     res.status(400).send({ msg: 'bad request' });
   } else if (err.code === '22003') {
@@ -19,6 +19,7 @@ app.use((err, req, res, next) => {
 
 // 500 errors
 app.use((err, req, res, next) => {
+  console.log(err);
   res.status(500).send({ msg: 'Internal Server Error' });
 });
 
