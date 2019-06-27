9fc4a28149dab3a935ec5d629b19da569cb2d494
Alice-Claire <aliceclaireh@hotmail.co.uk>
2019-06-27 11:26:34 +0100

    add handling for 400 error codes

--- app.js
+++ app.js
@@ -5,6 +5,18 @@ const apiRouter = require('./routers/apiRouter');
 app.use(express.json());
 app.use('/api', apiRouter);
 
+// handle psql 400 errors
+app.use((err, req, res, next) => {
+  const codes = ['22P02', '42703', '23502'];
+  if (codes.includes(err.code)) {
+    res.status(400).send({ msg: 'bad request' });
+  } else if (err.code === '22003') {
+    res.status(422).send({ msg: 'unprocessable entity' });
+  } else {
+    next(err);
+  }
+});
+
 // 500 errors
 app.use((err, req, res, next) => {
   res.status(500).send({ msg: 'Internal Server Error' });
