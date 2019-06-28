2669d49ce84c1b597966621a73c4e900c8c9b2a8
Alice-Claire <aliceclaireh@hotmail.co.uk>
2019-06-28 15:53:15 +0100

    extract error handling to seperate file

--- app.js
+++ app.js
@@ -1,26 +1,17 @@
 const express = require('express');
 const app = express();
 const apiRouter = require('./routers/apiRouter');
+const {
+  handleCustomErrors,
+  handlePsqlErrors,
+  handleServerErrors
+} = require('./errors');
 
 app.use(express.json());
 app.use('/api', apiRouter);
 
-// handle psql 400 errors
-app.use((err, req, res, next) => {
-  const codes = ['22P02', '23502', '23503', '42703'];
-  if (codes.includes(err.code)) {
-    res.status(400).send({ msg: 'bad request' });
-  } else if (err.code === '22003') {
-    res.status(422).send({ msg: 'unprocessable entity' });
-  } else {
-    next(err);
-  }
-});
-
-// 500 errors
-app.use((err, req, res, next) => {
-  console.log(err);
-  res.status(500).send({ msg: 'Internal Server Error' });
-});
+app.use(handleCustomErrors);
+app.use(handlePsqlErrors);
+app.use(handleServerErrors);
 
 module.exports = app;
--- errors.js
+++ errors.js
@@ -1,3 +1,23 @@
 exports.send405Error = (req, res, next) => {
   res.status(405).send({ msg: 'method not allowed' });
 };
+
+exports.handleCustomErrors = (err, req, res, next) => {
+  if (err.status) res.status(err.status).send({ msg: err.msg });
+  else next(err);
+};
+
+// handle psql 400 errors
+exports.handlePsqlErrors = (err, req, res, next) => {
+  const psqlCodes = ['22P02', '23502', '23503', '42703'];
+  if (psqlCodes.includes(err.code)) {
+    res.status(400).send({ msg: err.msg || 'bad request' });
+  } else if (err.code === '22003') {
+    res.status(422).send({ msg: err.msg || 'unprocessable entity' });
+  } else next(err);
+};
+
+exports.handleServerErrors = (err, req, res, next) => {
+  console.log(err);
+  res.status(500).send({ msg: 'Internal Server Error' });
+};
