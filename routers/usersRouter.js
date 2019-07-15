bae642fcc973d1eaf236f264c2dda8a2b360f10f
Alice-Claire <aliceclaireh@hotmail.co.uk>
2019-07-15 11:48:51 +0100

    amend users controller name

--- routers/usersRouter.js
+++ routers/usersRouter.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const usersRouter = express.Router();
-const { getUserByUsername } = require('../controllers/users');
+const { getUsers } = require('../controllers/users');
 const { send405Error } = require('../errors');
 
 usersRouter
   .route('/:username')
-  .get(getUserByUsername)
+  .get(getUsers)
   .all(send405Error);
 
 module.exports = usersRouter;
