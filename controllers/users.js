ac44386f5e8e3d5ce28f6800bd0a51cf5bd531b6
Alice-Claire <aliceclaireh@hotmail.co.uk>
2019-07-15 11:47:39 +0100

    amend logic to get all or 1 user, or status 404

--- controllers/users.js
+++ controllers/users.js
@@ -1,7 +1,16 @@
 const { fetchUser } = require('../models/users');
 
-exports.getUserByUsername = (req, res, next) => {
+exports.getUsers = (req, res, next) => {
   fetchUser(req.params)
-    .then(user => res.status(200).send({ user }))
+    .then(users => {
+      if (users.length < 1) {
+        res.status(404).send({ msg: 'not found' });
+      } else if (users.length === 1) {
+        const user = users[0];
+        res.status(200).send({ user });
+      } else {
+        res.status(200).send({ users });
+      }
+    })
     .catch(next);
 };
