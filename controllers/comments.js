05459250f752b6474db39ee01c0a4d6aff8e53eb
Alice-Claire <aliceclaireh@hotmail.co.uk>
2019-07-15 11:45:04 +0100

    add comment id check and 404 msgs

--- controllers/comments.js
+++ controllers/comments.js
@@ -2,15 +2,26 @@ const {
   createComment,
   fetchComments,
   updateComment,
-  destroyComment
+  destroyComment,
+  checkCommentId
 } = require('../models/comments');
+const { checkArticleId } = require('../models/articles');
 
 exports.postComment = (req, res, next) => {
   const { article_id } = req.params;
   const newComment = req.body;
-  createComment(article_id, newComment)
-    .then(addedComment => {
-      res.status(201).send({ comment: addedComment[0] });
+
+  checkArticleId(article_id)
+    .then(article => {
+      if (article.length > 0) {
+        createComment(article_id, newComment)
+          .then(addedComment => {
+            res.status(201).send({ comment: addedComment[0] });
+          })
+          .catch(next);
+      } else {
+        res.status(404).send({ msg: 'not found' });
+      }
     })
     .catch(next);
 };
@@ -27,19 +38,36 @@ exports.getCommentsByArticleId = (req, res, next) => {
 
 exports.patchComment = (req, res, next) => {
   const { comment_id } = req.params;
-  const inc_votes = req.body.inc_votes;
-  updateComment(comment_id, inc_votes)
-    .then(patchedComment => {
-      res.status(200).send({ comment: patchedComment[0] });
+  const inc_votes = req.body.inc_votes || 0;
+
+  checkCommentId(comment_id)
+    .then(comment => {
+      if (comment.length > 0) {
+        updateComment(comment_id, inc_votes)
+          .then(patchedComment => {
+            res.status(200).send({ comment: patchedComment[0] });
+          })
+          .catch(next);
+      } else {
+        res.status(404).send({ msg: 'not found' });
+      }
     })
     .catch(next);
 };
 
 exports.deleteComment = (req, res, next) => {
   const { comment_id } = req.params;
-  destroyComment(comment_id)
-    .then(() => {
-      res.status(204).send({});
+  checkCommentId(comment_id)
+    .then(comment => {
+      if (comment.length > 0) {
+        destroyComment(comment_id)
+          .then(() => {
+            res.status(204).send({});
+          })
+          .catch(next);
+      } else {
+        res.status(404).send({ msg: 'not found' });
+      }
     })
     .catch(next);
 };
